bd3c4a3850df824162d938d4703183fe75f74a18
jboucard <jab809@scarletmail.rutgers.edu>
2017-09-29 19:29:23 -0400

    Chapter 2, challenge 1

---
+++ ch_2/PC_2.java
@@ -0,0 +1,15 @@
+public class PC_2
+{
+	public static void main(String[] args)
+	{
+		String name;
+		int age;
+		double annualPay;
+
+		name = "Jon";
+		age = 14;
+		annualPay = 12.34;
+
+		System.out.println(name+" is "+age+" years old and makes $"+annualPay+" a year");
+	}
+}
