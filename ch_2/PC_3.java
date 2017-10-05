f0cbe42319814d30c41625fd7d5418a3fe6b222f
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-04 20:55:10 -0400

    Chapter 2, challenge 3 edit

---
+++ ch_2/PC_3.java
@@ -0,0 +1,19 @@
+public class PC_3
+{
+	public static void main(String[] args)
+	{
+		String firstName, lastName, street, city, state, zip, phone, major;
+
+		firstName = "Chase";
+		lastName = "Harrow";
+
+		street = "193 Mary Ct.";
+		city = "Bridgewater";
+		state = "NJ";
+		zip = "08807";
+		phone = "0123456789";
+		major = "Computer Science";
+
+		System.out.println("My name is "+firstName+" "+lastName+"\nI live at "+street+" "+city+" "+state+" "+zip+"\nI can be reached at "+phone+"\nCurrently I am studying "+major);
+	}
+}
