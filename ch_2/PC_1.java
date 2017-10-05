f3f148121d14b8cbe1a97488d23eee2ad2d8055a
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-04 20:38:27 -0400

    Chapter 2, challenge 1 edit

---
+++ ch_2/PC_1.java
@@ -0,0 +1,15 @@
+public class PC_1
+{
+	public static void main(String[] args)
+	{
+		String name;
+		int age;
+		double annualPay;
+
+		name = "Jon Bouc";
+		age = 14;
+		annualPay = 12.34;
+
+		System.out.println("My name is "+name+", my age is "+age+" and\nI hope to earn $"+annualPay+" per year");
+	}
+}
