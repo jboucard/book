a230acb59b235d5fd6092c3c9ded8f369db59b4f
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-12 17:26:12 -0400

    a partially complete program. Needs to go down the correct way

--- ch_2/PC_4.java
+++ ch_2/PC_4.java
@@ -1,101 +1,65 @@
-public class PC_4
-{
-	public static void main(String[] args) 
-	{
-		int i=0;
-		int j=0;
-		int k=7;
-		int middle = -1;
-		boolean up = true;
-		boolean line = false;
-		//while((i != 4)&&(j != 0))
-		{
-
-			
-
-			
-
-			while(i < k)
-			{
-
-
-				if((k%2)==1)
-				{
-					if(i == (k/2)+1)
-					{	
-						middle = i;
-					}
-				}else{
-					if(i == (k/2))
-					{
-						middle = i;
-					}
-					
-				}
-				//System.out.println(i+" "+j);
-				j=0;
-				
-
-				if(i==middle)
-				{
-					//System.out.print("middle: "+middle);	
-					up = false;
-				}
-
-
-				
-
-				/*while(up && j<k)
-				{
-					
-					System.out.print("*");
-					j+=2;
-					line = true;
-				}*/
-				/*while(up && (j<(i*2)))	
-				{
-					System.out.print("*");
-					j++;
-					line = true;
-					
-				}*/
-				/*while(!up && (j<(i*2)))	
-				{
-					System.out.print("*");
-					j++;
-					line = true;
-				}*/
-
-				while((up)&&(j<=i))
-				{
-					//System.out.print("1111111111111111");
-					System.out.print("*");
-					j++;
-					line = true;
-				}
-
-				//while((!up)&&(j<(k-(i/2))+1))
-				while((!up)&&(j<(k-(middle)-j)))
-				//while((!up)&&(j<=middle))	
-				{
-					//System.out.print(i+" "+j+" "+k);
-					//System.out.print("2222222222222222");
-					System.out.print("*");
-					j++;
-					line = true;
-				}
-
-
-
-				if(line)
-				{
-					System.out.println();	
-					line = false;
-				}
-				//System.out.print("*");
-				//System.out.println();
-				i++;
-			}
-		}
-	}
+public class PC_4{
+ 
+     public static void main(String []args){
+        int lines = 11;
+        int upStars, downStars, stars=1;
+        int i = 1, j=1;
+        boolean newLine = false;
+        int middle;
+       
+        //find middle line of diamond
+        if(lines%2 == 1)
+        {
+            middle = (lines/2)+1;
+        }else{
+            middle = lines/2;
+        }
+        //System.out.print("middle: "+middle+"\n");
+        while(i<=lines)
+        {
+            //System.out.println("i: "+i);
+            upStars=1;
+            downStars=1;
+           
+            if(i<=middle)
+            {
+                upStars = (i*2)-1;
+                stars = upStars;
+            }else{
+                stars-=2;
+            }
+            j=i;
+            while((i<=middle)&&(upStars != 0))
+            {
+                while(j<middle)
+                {
+                    System.out.print(" ");
+                    j++;
+                }
+                System.out.print("*");
+                upStars--;
+                newLine = true;
+            }
+            downStars = stars;
+            //System.out.println("\nup: "+upStars+" down: "+downStars+" stars: "+stars);
+            while((i>middle)&&(downStars != 0))
+            {
+                while(j>middle)
+                {
+                    System.out.print(" ");
+                    j--;
+                }
+                System.out.print("*");
+                downStars--;
+                newLine = true;
+            }
+           
+            if(newLine)
+            {
+                System.out.println();
+                newLine = false;
+            }
+            i++;
+        }
+     }
 }
