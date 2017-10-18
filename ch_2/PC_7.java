7bfc3f2bafe2c4e40448bfc71d03d4c32379ae94
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-18 00:43:50 -0400

    Calculates the total number of acres in lot of land measured in squarefeet

---
+++ ch_2/PC_7.java
@@ -0,0 +1,14 @@
+public class PC_7
+{
+	public static void main(String []args)
+	{
+		long totalLand = 389767;
+		long acre = 43560;
+		double acreNumb;
+
+		acreNumb = totalLand/acre;
+
+		System.out.println("The total number of acres are: "+acreNumb);
+
+	}
+}
