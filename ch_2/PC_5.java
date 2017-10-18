75e4da67bab7337e40b1e9f656ee541cfb168c32
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-18 00:16:56 -0400

    Calculates the sum of two numbers

---
+++ ch_2/PC_5.java
@@ -0,0 +1,13 @@
+public class PC_5
+{
+	public static void main(String []args)
+	{
+		int firstNum = 62;
+		int secondNum = 99;
+		int sum;
+
+		sum = firstNum + secondNum;
+
+		System.out.println("The sum of "+firstNum+" and "+secondNum+" is "+sum);
+	}
+}
