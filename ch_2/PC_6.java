5777f686a11461d767608314a69bc51bed2b1e6c
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-18 00:28:40 -0400

    Calculates the percentage of sales from a particluar division

---
+++ ch_2/PC_6.java
@@ -0,0 +1,15 @@
+public class PC_6
+{
+	public static void main(String[] args) 
+	{
+		double totalSales = 4600000;	
+		double percent = .62;
+		double eastCoastSales;
+
+		eastCoastSales = /*totalSales -*/ (totalSales*percent);
+
+		System.out.println("East Coast Sales are responsible for $"+eastCoastSales);
+
+	}
+}
+		
