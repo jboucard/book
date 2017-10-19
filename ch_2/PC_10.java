9d56e2e02b51e8f31c7e236831e7c1693ce15377
jboucard <jab809@scarletmail.rutgers.edu>
2017-10-19 00:24:12 -0400

    Enter the number of test scores, then the scores and find the average

--- ch_2/PC_10.java
+++ ch_2/PC_10.java
@@ -4,22 +4,47 @@ public class PC_10
 {
 	public static void main(String []args)
 	{
-		String scoreOneString, scoreTwoString, scoreThreeString;
-		double average, scoreOneDouble, scoreTwoDouble, scoreThreeDouble, priceAddStateAddCounty;
-		scoreOneString = JOptionPane.showInputDialog("Enter the first test score");
+		String scores, scoreString, scoreOneString, scoreTwoString, scoreThreeString;
+		int scoresNumb;
+		double average, add, scoreDouble, scoreOneDouble, scoreTwoDouble, scoreThreeDouble, priceAddStateAddCounty;
+		scores = JOptionPane.showInputDialog("Enter the number of scores");
+		scoresNumb = Integer.parseInt(scores);
+
+		add = 0;
+		for(int i=0; i<scoresNumb; i++)
+		{
+			i+=1;
+			scoreString = JOptionPane.showInputDialog("Enter test score "+i);
+			scoreDouble = Double.parseDouble(scoreString);
+			i-=1;
+			add = add+scoreDouble;
+
+		}
+		/*scoreOneString = JOptionPane.showInputDialog("Enter the first test score");
 		scoreTwoString = JOptionPane.showInputDialog("Enter the second test score");
 		scoreThreeString = JOptionPane.showInputDialog("Enter the third test score");
-
-
+		
 		scoreOneDouble = Double.parseDouble(scoreOneString);
 		scoreTwoDouble = Double.parseDouble(scoreTwoString);
 		scoreThreeDouble = Double.parseDouble(scoreThreeString);
+		*/
 
-		average = (scoreOneDouble+scoreTwoDouble+scoreThreeDouble)/3;
+		average = add/scoresNumb;
 
-		System.out.println("The first test score is: "+scoreOneDouble+
+		//average = (scoreOneDouble+scoreTwoDouble+scoreThreeDouble)/scoresNumb;
+
+
+
+		/*System.out.println("The first test score is: "+scoreOneDouble+
 							"\nThe second test score is: "+scoreTwoDouble+
 							"\nThe third test score is: "+scoreThreeDouble+
 							"\nThe average score is: "+average);
+		*/
+
+		//for(int j=0, j<scoresNumb, j++)
+		{
+			System.out.println("The average score is: "+average);
+
+		}
 	}
 }
